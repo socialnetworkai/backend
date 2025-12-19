4c87483d5a9241634346149ec9d8581d0080f28d
Ilya Anisimkov <oblak.00@bk.ru>
2025-12-19 13:37:24 +0500

    fix

--- apps/main/src/main.ts
+++ apps/main/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api/v1');
+  app.setGlobalPrefix('/api/v1');
   console.log(`main application started on port ${process.env.PORT}`);
   await app.listen(process.env.PORT!);
 }
