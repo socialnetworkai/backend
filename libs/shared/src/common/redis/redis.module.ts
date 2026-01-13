8200ab017ec34c5b69ac2c6e0438a0b4bfd94375
backtobackend226 <backtobackend@proton.me>
2026-01-13 09:59:01 -0800

    feat: redis errror loging

--- libs/shared/src/common/redis/redis.module.ts
+++ libs/shared/src/common/redis/redis.module.ts
@@ -14,6 +14,11 @@ export class IoRedisModule {
           useFactory: (configService: ConfigService) => ({
             type: 'single',
             url: configService.get<string>('REDIS_URL'),
+            reconnectOnError: (error: Error) => {
+              console.error('Redis encountered an error:', error.message);
+              return true;
+            },
+            maxRetriesPerRequest: 20,
             options: {
               username: configService.get<string>('REDIS_USERNAME'),
               password: configService.get<string>('REDIS_PASSWORD'),
