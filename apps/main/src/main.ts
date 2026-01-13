bd411591f3fb698f4925bbc421567821598a588c
Ilya Anisimkov <oblak.00@bk.ru>
2026-01-13 16:36:28 +0500

    add new deployment.yaml

--- apps/main/src/main.ts
+++ apps/main/src/main.ts
@@ -20,12 +20,17 @@ async function bootstrap() {
 
   const logger = await app.resolve(LoggerService);
 
+  console.log(3);
   app.useGlobalFilters(
     new AllHttpExceptionsFilter(logger),
     new DomainHttpExceptionFilter(logger),
   );
 
+  console.log(4);
+
   app.use(cookieParser());
+
+  console.log(5);
 
   logger.log('MAIN', `main application started on port ${process.env.PORT}`);
   await app.listen(process.env.PORT || 3000);
