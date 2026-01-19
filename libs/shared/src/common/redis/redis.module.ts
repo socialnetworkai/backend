d41e799f6f3aa8e5833c2b7d5244ecfde3db0dec
backtobackend226 <backtobackend@proton.me>
2026-01-19 12:18:08 -0800

    fix: dependencies errors when install new dependencies only with deleting old outdated ioredis and installing the new one

--- libs/shared/src/common/redis/redis.module.ts
+++ libs/shared/src/common/redis/redis.module.ts
@@ -1,6 +1,6 @@
 import { DynamicModule, Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { RedisModule } from '@nestjs-modules/ioredis';
+import { RedisModule } from '@nestjs-modules-mo/ioredis';
 
 @Module({})
 export class IoRedisModule {
